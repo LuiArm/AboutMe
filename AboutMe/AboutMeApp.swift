97edd1771cb1b71b7b1804e240a3f0f58545e460
LuiArm <luisarmendariz24@icloud.com>
2024-06-11 21:06:25 -0600

    Initial Commit

---
+++ AboutMe/AboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeApp.swift
+//  AboutMe
+//
+//  Created by luis armendariz on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
